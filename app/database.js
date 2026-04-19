ac253315ef2f2342e1ab77f3478324eea49855e6
Abhishek001konni <Abhishek001konni@gmail.com>
2026-04-18 22:52:12 -0400

    fix deprecated command

--- app/database.js
+++ app/database.js
@@ -4,12 +4,12 @@ import { readFileSync, mkdirSync } from "fs";
 mkdirSync("data", { recursive: true });
 
 const db = new Database("data/app.db", { create: true });
-db.exec("PRAGMA foreign_keys = ON;");
-db.exec("PRAGMA journal_mode = WAL;");
+db.run("PRAGMA foreign_keys = ON;");
+db.run("PRAGMA journal_mode = WAL;");
 
 export function initDb() {
-  db.exec(readFileSync("db/schema.sql", "utf8"));
-  db.exec(readFileSync("db/seed.sql", "utf8"));
+  db.run(readFileSync("db/schema.sql", "utf8"));
+  db.run(readFileSync("db/seed.sql", "utf8"));
 }
 
 export default db;
