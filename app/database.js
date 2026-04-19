146f550f03a8ff4784ab604fe8f1264e387a1a48
Abhishek001konni <Abhishek001konni@gmail.com>
2026-04-18 22:29:20 -0400

    db: add SQLite connection and init function

---
+++ app/database.js
@@ -0,0 +1,13 @@
+import { Database } from "bun:sqlite";
+import { readFileSync } from "fs";
+
+const db = new Database("data/app.db", { create: true });
+db.exec("PRAGMA foreign_keys = ON;");
+db.exec("PRAGMA journal_mode = WAL;");
+
+export function initDb() {
+  db.exec(readFileSync("db/schema.sql", "utf8"));
+  db.exec(readFileSync("db/seed.sql", "utf8"));
+}
+
+export default db;
